bf9771776bf885d555cc41f0144d2a5425b1c23e
David Bohunek <david@bohunek.net>
2015-03-12 22:05:05 +0000

    Dist folder
    Dist folder is public now, min.js is built to that folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 ﻿var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
  
 gulp.task('default', function() {
   gulp.src('src/*.js')
     .pipe(uglify({mangle: false}))
+	.pipe(rename({
+		extname: '.min.js'
+	}))
     .pipe(gulp.dest('dist'))
 });
